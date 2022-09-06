0a73d0c4c2d9da24fc838a2e68e88bbd57fc2bad
AOsman0 <99136448+AOsman0@users.noreply.github.com>
2022-09-06 18:25:20 +0100

    fixed the high-scores-card

--- src/components/FlipCardScore.js
+++ src/components/FlipCardScore.js
@@ -1,50 +1,30 @@
-import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import { Grid } from "@mui/material";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 export const FlipCardScore = () => {
   return (
-    <CardContent
-      sx={{
-        backgroundColor: "teal",
-      }}
-    >
-      <Grid container direction="row" justifyContent="space-evenly">
+    <div>
+      <Grid container spacing={7} className="grid-container">
         <Grid item xs={4}>
-          <Typography
-            sx={{ fontSize: 10, fontFamily: "serif" }}
-            color="white"
-            gutterBottom
-          >
-            Hey <span className="wave">👋</span> , Osman You Scored
-            <br />
-            <div className="flip-scores-img"> </div>
-          </Typography>
-        </Grid>
-
-        <Grid item xs={4}>
-          <Typography
-            sx={{ fontSize: 10, fontFamily: "serif" }}
-            color="white"
-            gutterBottom
-          >
-            Hey <span className="wave">👋</span> , Osman You Scored
-            <br />
-            <div className="flip-scores-img"> </div>
-          </Typography>
-        </Grid>
-        <Grid item xs={4}>
-          <Typography
-            sx={{ fontSize: 10, fontFamily: "serif" }}
-            color="white"
-            gutterBottom
-          >
-            Hey <span className="wave">👋</span> , Osman You Scored
-            <br />
-            <div className="flip-scores-img"> </div>
-          </Typography>
+          <Card sx={{ maxWidth: 345, backgroundColor: "teal" }}>
+            <CardMedia
+              component="img"
+              height="140"
+              className="flip-scores-img"
+              alt="Flip Img"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Hey <span className="wave">👋</span> , Osman You Scored
+                <br />
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
-    </CardContent>
+    </div>
   );
 };
--- src/containers/HighScorePage.js
+++ src/containers/HighScorePage.js
@@ -1,31 +1,10 @@
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import { Fragment } from "react";
-
 import { FlipCardScore } from "../components/FlipCardScore";
 
 export const HighScorePage = () => {
-  const card = (
-    <Fragment>
-      <FlipCardScore />
-    </Fragment>
-  );
-
   return (
     <div className="high-scores-card">
       <div className="arrow-img">
-        <Box
-          sx={{
-            width: "50rem",
-            height: "rem",
-            textAlign: "center",
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-          }}
-        >
-          <Card variant="outlined">{card} </Card>
-        </Box>
+        <FlipCardScore />
       </div>
     </div>
   );
