838fb6d1c45cd1aad502374ac6939f83b4dc1f5c
AOsman0 <99136448+AOsman0@users.noreply.github.com>
2022-09-10 17:28:07 +0100

    rendered basic created quiz cards

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,24 +1,29 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import basicImage from "../images/scores.jpg";
 
 export const Quiz = ({ quizData }) => {
   console.log(quizData);
 
   return (
-    <div>
+    <div className="create-quiz-cards">
       {quizData.map((each) => {
         console.log(each.category);
         return (
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+          <Card style={{ width: "18rem" }} className="card-styles">
+            <Card.Img
+              variant="top"
+              src={basicImage}
+              style={{ width: "18rem", height: "10rem" }}
+            />
             <Card.Body>
-              <Card.Title>{each.category}</Card.Title>
-              <Card.Text>{each.question}</Card.Text>
-              <Card.Text>{each.correct_answer}</Card.Text>
-              <Card.Text>{each.incorrect_answers}</Card.Text>
-              <Card.Text>{each.difficulty}</Card.Text>
-              <Card.Text>{each.type}</Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <Card.Title>Category:{each.category}</Card.Title>
+              <Card.Text>Question :{each.question}</Card.Text>
+              <Card.Text>Correct Answer: {each.correct_answer}</Card.Text>
+              <Card.Text>Incorrect Answer :{each.incorrect_answers}</Card.Text>
+              <Card.Text>Difficulty Level :{each.difficulty}</Card.Text>
+              <Card.Text>Type :{each.type}</Card.Text>
+              <Button variant="primary">Save Quiz Into Dashboard</Button>
             </Card.Body>
           </Card>
         );
