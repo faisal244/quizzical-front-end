0ec88d9d869c4f5eff965ad0b79e0446cd211fab
smeea-2018 <smeeaarshad@gmail.com>
2022-09-10 14:38:06 +0100

    get all my quizzes

--- src/containers/ViewQuizPage.js
+++ src/containers/ViewQuizPage.js
@@ -1,58 +1,23 @@
 import { FlipCardScore } from "../components/FlipCardScore";
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
-import { CREATEQUIZ } from "../graphql/mutations";
-import axios from "axios";
-import { useMutation } from "@apollo/client";
+import { useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { GETMYQUIZES } from "../graphql/queries";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 export const ViewQuizPage = () => {
-  const location = useLocation();
-
-  const [
-    createQuiz,
-    { data: quizData, loading: quizLoading, error: quizError },
-  ] = useMutation(CREATEQUIZ);
-
-  useEffect(() => {
-    const categoryName = location.state.catName;
-    const difficulty = location.state.difficulty;
-    const questions = location.state.questions;
-    const localType = location.state.questionType;
-    // CONSTRUCT THE URL
-
-    const URL = `https://opentdb.com/api.php?amount=${questions}&category=${categoryName}&difficulty=${difficulty}&type=${localType}`;
-
-    // MAKE API CALL
-
-
-    const getResults = () => {
-      axios
-        .get(`${URL}`)
-        .then((response) => {
-          console.log(JSON.stringify(response));
-          // once we get the results we set them to state variables
-        })
-        .catch((error) => console.error(`ERROR: ${error}`));
-    };
-    // ONCE RESPONSE IS RETURNED
-    // SEND THE RESPONSE TO BACKEND
-// const responseData={
-//   type:
-//   difficulty:
-//   category:
-//   questions:{}
-// }
-    // call a function that makes azios request and sets all the states
-    createQuiz({
-      // variables: {responseData},
-    });
-  }, []);
+  const { data, loading, error } = useQuery(GETMYQUIZES);
 
   return (
-    <div className="high-scores-card">
-      <div className="arrow-img">
-        <FlipCardScore />
-      </div>
+    <div>
+      <pre>
+        <div>{JSON.stringify(data, null, 2)}</div>
+      </pre>
     </div>
   );
 };
