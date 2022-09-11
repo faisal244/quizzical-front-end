1e4528041762ea3b55bf75a61e623b5304d36bc3
Amalidi <A.idi12@outlook.com>
2022-09-11 18:41:17 +0100

    text

--- src/containers/Introduction.js
+++ src/containers/Introduction.js
@@ -4,11 +4,11 @@ export const Introduction = () => {
   return (
     <div>
       <Carousel>
-        <Carousel.Item interval={700}>
+        <Carousel.Item interval={1000}>
           <img
             className="d-block w-100"
             height={708}
-            src="https://media.istockphoto.com/photos/and-a-concept-yellow-question-mark-glowing-amid-black-question-marks-picture-id1305169776?b=1&k=20&m=1305169776&s=170667a&w=0&h=mpYdh2MzGN_rqxoRNlO5KWGCCq3ZktzSfp-wA0nD9no="
+            src="https://tufnellpark.islington.sch.uk/wp-content/uploads/2021/01/quiz-banner-1.jpg"
             alt="First slide"
           />
           <Carousel.Caption>
@@ -16,30 +16,28 @@ export const Introduction = () => {
             <p>A web based virtual quiz you can play anywhere at anytime .</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={900}>
+        <Carousel.Item interval={1000}>
           <img
             className="d-block w-100"
             height={708}
-            src="https://media.istockphoto.com/photos/yellow-question-mark-on-a-background-of-black-signs-faq-concept-picture-id1308986719?b=1&k=20&m=1308986719&s=170667a&w=0&h=-vJ0pQHGn5CZcvzETX6PhRQ8I30z5--gNR93BDFVgxo="
+            src="https://c1.wallpaperflare.com/preview/653/576/854/question-mark-pile-question-mark.jpg"
             alt="Second slide"
           />
           <Carousel.Caption>
-            <h3>Various Question Types</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Intuitive Quiz Builder</h3>
+            <p>quiz builder that allows you to choose the questions.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={900}>
+        <Carousel.Item interval={1000}>
           <img
             className="d-block w-100"
             height={708}
-            src="https://images.unsplash.com/photo-1541339907198-e08756dedf3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            src="https://images.unsplash.com/photo-1516321497487-e288fb19713f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
             alt="Third slide"
           />
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h3>Thousands of people. One Big Quiz.</h3>
+            <p>Bring out the competitive spirit</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
