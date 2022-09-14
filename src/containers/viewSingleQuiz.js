5f4a37b47b2da31feca7d6263bac3824685ea977
Faisal Mohammed <m.faisal244@gmail.com>
2022-09-14 20:02:27 +0100

    Cleaned up code in Ap.js

--- src/containers/viewSingleQuiz.js
+++ src/containers/viewSingleQuiz.js
@@ -9,42 +9,48 @@ import { GETSINGLEQUIZ } from "graphql/queries";
 import { DELETEQUIZ } from "../graphql/mutations";
 
 export const ViewSingleQuizPage = () => {
-  const navigate = useNavigate();
-  const [
-    deleteQuiz,
-    { data: deletedData, loading: deletedLoading, error: deletedError },
-  ] = useMutation(DELETEQUIZ);
+	const navigate = useNavigate();
+	const [
+		deleteQuiz,
+		{ data: deletedData, loading: deletedLoading, error: deletedError },
+	] = useMutation(DELETEQUIZ);
 
-  const { id } = useParams();
+	const { id } = useParams();
 
-  const handleDeleteQuiz = async (event) => {
-    event.preventDefault();
-    const deletedQuiz = await deleteQuiz({ variables: { deleteQuizId: id } });
+	const handleDeleteQuiz = async (event) => {
+		event.preventDefault();
+		const deletedQuiz = await deleteQuiz({ variables: { deleteQuizId: id } });
 
-    navigate("/dashboard");
-  };
+		navigate("/dashboard");
+	};
 
-  const { data, loading, error } = useQuery(GETSINGLEQUIZ, {
-    variables: { getQuizId: id },
-  });
+	const { data, loading, error } = useQuery(GETSINGLEQUIZ, {
+		variables: { getQuizId: id },
+	});
 
-  return (
-    data && (
-      <div>
-        {<TableOfQuestions quizData={data.getQuiz.questions} />}
-        <Box m={1} display="flex" justifyContent="center" alignItems="center">
-          <Button
-            variant="contained"
-            color="error"
-            sx={{ justifyContent: "center" }}
-            onClick={handleDeleteQuiz}
-          >
-            Delete Quiz
-          </Button>
-        </Box>
-      </div>
-    )
-  );
+	return (
+		data && (
+			<div>
+				{<TableOfQuestions quizData={data.getQuiz.questions} />}
+				<Box
+					m={1}
+					display="flex"
+					justifyContent="center"
+					alignItems="center"
+				>
+					<Button
+						variant="contained"
+						color="error"
+						sx={{ justifyContent: "center" }}
+						onClick={handleDeleteQuiz}
+					>
+						Delete Quiz
+					</Button>
+				</Box>
+			</div>
+		)
+	);
 };
 
 export default ViewSingleQuizPage;
+//
