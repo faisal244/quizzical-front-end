42d8ead9e7cb162660a6ea04be7fa0dc133740ad
AOsman0 <99136448+AOsman0@users.noreply.github.com>
2022-09-14 19:35:14 +0100

    deletedunusedcode

--- src/containers/ViewAllQuizzesPage.js
+++ src/containers/ViewAllQuizzesPage.js
@@ -1,15 +1,14 @@
-// import Hero from "components/hero/FullWidthWithImage";
 import Card from "components/cards/TabCardGrid.js";
 import AnimationRevealPage from "helpers/AnimationRevealPage";
 import { default as Footer } from "components/footers/MiniCenteredFooter";
 
 export const ViewAllQuizzes = () => {
-	return (
-		<AnimationRevealPage>
-			<Card heading={"ALL QUIZZES"} />
-			<Footer />
-		</AnimationRevealPage>
-	);
+  return (
+    <AnimationRevealPage>
+      <Card heading={"ALL QUIZZES"} />
+      <Footer />
+    </AnimationRevealPage>
+  );
 };
 
 export default ViewAllQuizzes;
