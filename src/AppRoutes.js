6fa071115255db711ab465a72f87b2b38efcd57a
AOsman0 <99136448+AOsman0@users.noreply.github.com>
2022-09-14 20:23:35 +0100

    viewsinglepage

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,13 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "./containers/LoginPage";
 import { SignUpPage } from "./containers/SignUpPage";
 import { CreateQuizPage } from "./containers/CreateQuizPage";
-import { Introduction } from "./containers/Introduction";
 import Hero from "components/hero/FullWidthWithImage";
 import { useAuth } from "./context/AppProvider";
 import { Homepage } from "./containers/Homepage";
 import { ViewAllQuizzes } from "./containers/ViewAllQuizzesPage";
 import { Footer } from "components/footers/MiniCenteredFooter";
-import { default as ViewSingleQuizPage } from "containers/ViewSingleQuiz";
+import { default as ViewSingleQuizPage } from "containers/ViewSingleQuizPage";
 
 export const AppRoutes = () => {
   const { isLoggedIn } = useAuth();
---
+++ src/containers/ViewSingleQuizPage.js
@@ -0,0 +1,50 @@
+import TableOfQuestions from "components/TableOfQuestions";
+import Button from "@mui/material/Button";
+import { Box } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { useMutation } from "@apollo/client";
+import { GETSINGLEQUIZ } from "graphql/queries";
+import { DELETEQUIZ } from "../graphql/mutations";
+
+export const ViewSingleQuizPage = () => {
+  const navigate = useNavigate();
+  const [
+    deleteQuiz,
+    { data: deletedData, loading: deletedLoading, error: deletedError },
+  ] = useMutation(DELETEQUIZ);
+
+  const { id } = useParams();
+
+  const handleDeleteQuiz = async (event) => {
+    event.preventDefault();
+    const deletedQuiz = await deleteQuiz({ variables: { deleteQuizId: id } });
+
+    navigate("/dashboard");
+  };
+
+  const { data, loading, error } = useQuery(GETSINGLEQUIZ, {
+    variables: { getQuizId: id },
+  });
+
+  return (
+    data && (
+      <div>
+        {<TableOfQuestions quizData={data.getQuiz.questions} />}
+        <Box m={1} display="flex" justifyContent="center" alignItems="center">
+          <Button
+            variant="contained"
+            color="error"
+            sx={{ justifyContent: "center" }}
+            onClick={handleDeleteQuiz}
+          >
+            Delete Quiz
+          </Button>
+        </Box>
+      </div>
+    )
+  );
+};
+
+export default ViewSingleQuizPage;
